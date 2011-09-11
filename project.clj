1daec3b0584b04b22b05ae9e40c7fadc3ceee0a9
Josh Comer <joshjcomer@gmail.com>
2011-09-11 16:06:15 -0300

    Updated cloJars version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject trollscript "1.0.0"
   :description "A trollscript interpreter in clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]
+  :dev-dependencies [[lein-clojars "0.7.0"]
                      [lein-marginalia "0.6.1"]]
   :main trollscript.core)
