45f9bd40238d01fcdac68a0b102dd72ad816b741
Josh Comer <joshjcomer@gmail.com>
2011-09-11 15:38:57 -0300

    Initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject trollscript "1.0.0-SNAPSHOT"
+  :description "A trollscript interpreter in clojure"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :main trollscript.core)
---
+++ test/trollscript/test/core.clj
@@ -0,0 +1,6 @@
+(ns trollscript.test.core
+  (:use [trollscript.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
